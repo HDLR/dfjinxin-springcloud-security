c6de5d51cdb7080f783fb15234607822bd271b58
HDLR <492587402@qq.com>
2019-05-14 15:26:40 +0800

    更改spring cloud alibaba版本

--- dfjinxin-springcloud-security/dfjinxin-monitor/src/main/java/com/dfjinxin/monitor/nacos/NacosWatchAutoConfiguration.java
+++ dfjinxin-springcloud-security/dfjinxin-monitor/src/main/java/com/dfjinxin/monitor/nacos/NacosWatchAutoConfiguration.java
@@ -1,18 +1,18 @@
-package com.dfjinxin.monitor.nacos;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.alibaba.nacos.NacosDiscoveryProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class NacosWatchAutoConfiguration {
-
-    @Bean
-    @ConditionalOnMissingBean
-    @ConditionalOnProperty(value = "spring.cloud.nacos.discovery.watch.enabled", matchIfMissing = true)
-    public NacosWatch nacosWatch(NacosDiscoveryProperties nacosDiscoveryProperties) {
-        return new NacosWatch(nacosDiscoveryProperties);
-    }
-}
+//package com.dfjinxin.monitor.nacos;
+//
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.cloud.alibaba.nacos.NacosDiscoveryProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class NacosWatchAutoConfiguration {
+//
+//    @Bean
+//    @ConditionalOnMissingBean
+//    @ConditionalOnProperty(value = "spring.cloud.nacos.discovery.watch.enabled", matchIfMissing = true)
+//    public NacosWatch nacosWatch(NacosDiscoveryProperties nacosDiscoveryProperties) {
+//        return new NacosWatch(nacosDiscoveryProperties);
+//    }
+//}
